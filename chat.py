f17468a0c83872b9319f8a21513c40efd244ee41
Rohit2216 <chauhanrohit716@gmail.com>
2023-09-01 22:39:23 +0530

    chat with pdf done

---
+++ chat.py
@@ -0,0 +1,60 @@
+# chat.py
+
+import streamlit as st
+import pickle
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+from dotenv import load_dotenv
+
+
+
+
+def app():
+    st.header("Chat with PDF 💬")
+    load_dotenv()
+    
+    # Upload a PDF file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
+
+        store_name = pdf.name[:-4] if pdf.name else "default"  # Handle the case when pdf.name is None
+
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                VectorStore = pickle.load(f)
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+
+        query = st.text_input("Ask Questions About your Pdf File:")
+
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+            llm = OpenAI(model_name='gpt-3.5-turbo')
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+            st.write(response)
+
+if __name__ == '__main__':
+    app()
